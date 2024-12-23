6b6909012c6e8ad489c1fa42b770dcf1a69c95e3
sizuichu <1625621257@qq.com>
2024-12-23 19:51:57 +0800

    【优化】新增转换母版设置

--- Wpf_Form/Wpf_MasterSet.xaml.cs
+++ Wpf_Form/Wpf_MasterSet.xaml.cs
@@ -1,59 +1,54 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using HandyControl.Controls;
+using MessageBox = HandyControl.Controls.MessageBox;
+using Window = HandyControl.Controls.Window;
 
 namespace PresPio
-    {
-    /// <summary>
-    /// Wpf_MasterSet.xaml 的交互逻辑
-    /// </summary>
-    public partial class Wpf_MasterSet
+{
+    public partial class Wpf_MasterSet 
     {
         public Wpf_MasterSet()
         {
             InitializeComponent();
+            LoadSettings();
         }
 
-        private void MastWindown_Loaded(object sender, RoutedEventArgs e)
+        private void LoadSettings()
         {
-            float Fsize = Properties.Settings.Default.Pla_size;
-            bool N1 = Properties.Settings.Default.Pla_N1;
-            bool N2 = Properties.Settings.Default.Pla_N2;
-            bool N3 = Properties.Settings.Default.Pla_N3;
-            bool N4 = Properties.Settings.Default.Pla_N4;
-            numericUpDown.Value = (int)Fsize;
-            uiCheckBox1.IsChecked = N1;
-            uiCheckBox2.IsChecked = N2;
-            uiCheckBox3.IsChecked = N3;
-            uiCheckBox4.IsChecked = N4;
+            // 从设置中加载选项状态
+            DeleteSourceCheck.IsChecked = Properties.Settings.Default.Pla_N1;
+            SkipShapeCheck.IsChecked = Properties.Settings.Default.Pla_N2;
+            KeepTextCheck.IsChecked = Properties.Settings.Default.Pla_N3;
+            SkipPictureCheck.IsChecked = Properties.Settings.Default.Pla_N4;
         }
 
-        private void uiCheckBox1_Checked(object sender, RoutedEventArgs e)
+        private void SaveSettings()
         {
-            Properties.Settings.Default.Pla_N1 = (bool)uiCheckBox1.IsChecked;
+            // 保存设置到配置文件
+            Properties.Settings.Default.Pla_N1 = DeleteSourceCheck.IsChecked ?? false;
+            Properties.Settings.Default.Pla_N2 = SkipShapeCheck.IsChecked ?? false;
+            Properties.Settings.Default.Pla_N3 = KeepTextCheck.IsChecked ?? false;
+            Properties.Settings.Default.Pla_N4 = SkipPictureCheck.IsChecked ?? false;
             Properties.Settings.Default.Save();
         }
 
-        private void uiCheckBox2_Checked(object sender, RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            Properties.Settings.Default.Pla_N2 = (bool)uiCheckBox2.IsChecked;
-            Properties.Settings.Default.Save();
+            SaveSettings();
+            Growl.Success("设置已保存");
         }
 
-        private void uiCheckBox3_Checked(object sender, RoutedEventArgs e)
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            Properties.Settings.Default.Pla_N3 = (bool)uiCheckBox3.IsChecked;
-            Properties.Settings.Default.Save();
+            SaveSettings();
+            Close();
         }
 
-        private void uiCheckBox4_Checked(object sender, RoutedEventArgs e)
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
-            Properties.Settings.Default.Pla_N4 = (bool)uiCheckBox4.IsChecked;
-            Properties.Settings.Default.Save();
-        }
-
-        private void numericUpDown_ValueChanged(object sender, HandyControl.Data.FunctionEventArgs<double> e)
-        {
-            Properties.Settings.Default.Pla_size = (float)numericUpDown.Value;
-            Properties.Settings.Default.Save();
+            SaveSettings();
+            base.OnClosing(e);
         }
     }
 }
